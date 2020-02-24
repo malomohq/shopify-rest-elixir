08c138cc44f598c0e8e8599154f1599f69420e70
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-24 15:57:06 -0500

    Created struct for HTTP response

---
+++ lib/shopify/rest/response.ex
@@ -0,0 +1,26 @@
+defmodule Shopify.REST.Response do
+  alias Shopify.REST.{ Client, Config, Helpers }
+
+  @type t ::
+          %__MODULE__{
+            body: map,
+            headers: [{ String.t(), String.t() }],
+            private: map,
+            status_code: pos_integer
+          }
+
+  defstruct body: nil,
+            headers: nil,
+            private: %{},
+            status_code: nil
+
+  @spec new(Client.response_t(), Config.t(), map) :: t
+  def new(response, config, private) do
+    %__MODULE__{
+      body: Helpers.JSON.decode(response.body, config),
+      headers: response.headers,
+      private: private,
+      status_code: response.status_code
+    }
+  end
+end
