b5b39c0dafeb6221278ef1bf8486b74d4ace5397
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-02-24 16:36:05 -0500

    Added bypass as a test dependency

--- mix.exs
+++ mix.exs
@@ -30,7 +30,13 @@ defmodule Shopify.REST.MixProject do
       #
 
       { :dialyxir, "~> 1.0-rc", only: :dev, runtime: false },
-      { :ex_doc, ">= 0.0.0", only: :dev, runtime: false }
+      { :ex_doc, ">= 0.0.0", only: :dev, runtime: false },
+
+      #
+      # test
+      #
+
+      { :bypass, "~> 1.0", only: :test }
     ]
   end
 
