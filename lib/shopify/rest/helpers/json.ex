9211fd4a1b94ed62423a0d8edded43551ccd8c77
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-04-10 10:49:57 -0400

    Allow encoding nil values

--- lib/shopify/rest/helpers/json.ex
+++ lib/shopify/rest/helpers/json.ex
@@ -13,12 +13,7 @@ defmodule Shopify.REST.Helpers.JSON do
     end
   end
 
-
   @spec encode(map, Config.t()) :: String.t()
-  def encode(nil, _config) do
-    ""
-  end
-  
   def encode(map, config) do
     case config.json_codec.encode(map) do
       { :ok, result } ->
