bbd871dcc6d5bfb5c1aaeef801972ff64cf23ad9
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-04-11 14:46:25 -0400

    Bump version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shopify.REST.MixProject do
   def project do
     [
       app: :shopify_rest,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
